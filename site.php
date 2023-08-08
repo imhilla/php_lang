6cf6bdbbabefc32ae7423ee6e75ce3797cd2d7cb
imhilla <hillaryodhiambo282@gmail.com>
2023-08-08 11:04:42 +0300

    updates

---
+++ site.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  echo ("Hello world");
+  echo "<h1>Document</h1>";
+  echo "<hr>";
+  $age = 23;
+  $name = "John Smith";
+  echo $name;
+  echo "$age hello world <br>";
+  $phrase = "To be or not to be <br>";
+  echo $phrase;
+  ?>
+</body>
+
+</html>
